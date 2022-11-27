af8f7ed4f57dbe4e5cf062feaf87baa8bd232ce8
TTania <>
2022-11-27 14:53:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Display print function to the standard output
+#The argument is placed between the round brackets. If the argument is a text string then shoul be additionally placed inside quotes to identify it as a string.
+#The print function is a built-in function, and it is part of the standard python library.
+print("System Failure Imminent!")
+print("                        ")
+print("Print function works good.")
